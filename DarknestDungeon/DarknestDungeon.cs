2c30e69e18005c1185020febe6ab53668391cc1a
dplochcoder <daniel.ploch.coder@gmail.com>
2022-09-24 04:04:51 -0400

    Reorganize solution

---
+++ DarknestDungeon/DarknestDungeon.cs
@@ -0,0 +1,14 @@
+﻿using Modding;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace DarknestDungeon
+{
+    public class DarknestDungeon : Mod
+    {
+        public DarknestDungeon() : base("Darknest Dungeon") { }
+
+        public override string GetVersion() => Version.Instance;
+    }
+}
