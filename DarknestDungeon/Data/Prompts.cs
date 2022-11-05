82a508700380fc24ca309f146bcd19bfe18b9c61
dplochcoder <daniel.ploch.coder@gmail.com>
2022-11-05 14:36:03 -0400

    Add prompts module

---
+++ DarknestDungeon/Data/Prompts.cs
@@ -0,0 +1,20 @@
+﻿using DarknestDungeon.IC;
+using ItemChanger;
+using System.Collections.Generic;
+
+namespace DarknestDungeon.Data
+{
+    using JsonUtil = PurenailCore.SystemUtil.JsonUtil<DarknestDungeon>;
+
+    public static class Prompts
+    {
+        public static readonly SortedDictionary<string, string> Data = JsonUtil.DeserializeEmbedded<SortedDictionary<string, string>>("DarknestDungeon.Resources.Data.prompts.json");
+
+        public static void Load()
+        {
+            if (DarknestDungeon.Instance != null) DarknestDungeon.Log("Prompts loaded");
+        }
+
+        public static void Install() => ItemChangerMod.Modules.Add<PromptsModule>();
+    }
+}
---
+++ DarknestDungeon/IC/PromptsModule.cs
@@ -0,0 +1,14 @@
+﻿using DarknestDungeon.Data;
+using Modding;
+
+namespace DarknestDungeon.IC
+{
+    public class PromptsModule : ItemChanger.Modules.Module
+    {
+        public override void Initialize() => ModHooks.LanguageGetHook += OverrideLanguageGet;
+
+        public override void Unload() => ModHooks.LanguageGetHook -= OverrideLanguageGet;
+
+        private string OverrideLanguageGet(string key, string sheetTitle, string orig) => Prompts.Data.TryGetValue(key, out var value) ? value : orig;
+    }
+}
