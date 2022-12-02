b2a98022e793127b9250b6c78efbdcbfe6a52676
dplochcoder <daniel.ploch.coder@gmail.com>
2022-12-01 21:04:37 -0500

    Generic state machine

---
+++ DarknestDungeon/Enemy/EnemyState.cs
@@ -0,0 +1,18 @@
+﻿namespace DarknestDungeon.Enemy
+{
+    public class EnemyState<S, T> where T : EnemyState<S, T>
+    {
+        public readonly EnemyStateMachine<S, T> mgr;
+
+        public EnemyState(EnemyStateMachine<S, T> mgr)
+        {
+            this.mgr = mgr;
+        }
+
+        public virtual void Init() { }
+
+        public virtual void Update() { }
+
+        public virtual void Stop() { }
+    }
+}
