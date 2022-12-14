4d85533aadd7d37e23d1ba040d6324bc792b91a2
dplochcoder <daniel.ploch.coder@gmail.com>
2022-12-14 00:24:45 -0500

    Start implementing tilemap compiler

---
+++ DarknestDungeonUnityScripts/Lib/ColliderOptimizer.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DarknestDungeon.Lib
+{
+    public interface Grid
+    {
+        int Width();
+        int Height();
+        bool Filled(int x, int y);
+    }
+
+    public class Rect
+    {
+        public int X;
+        public int Y;
+        public int W;
+        public int H;
+    }
+
+    internal class CoverageRuns
+    {
+        private readonly int width;
+        private readonly int height;
+        private int[,] xRuns;
+        private int[,] yRuns;
+        private bool[,] claimed;
+        private int cX = 0;
+        private int cY = 0;
+
+        public CoverageRuns(Grid g)
+        {
+            this.width = g.Width();
+            this.height = g.Height();
+            this.xRuns = new int[width, height];
+            this.yRuns = new int[width, height];
+            this.claimed = new bool[width, height];
+
+            for (int x = 0; x < width; x++)
+            {
+                int runSize = 0;
+                for (int y = height - 1; y >= 0; y--)
+                {
+                    if (g.Filled(x, y)) ++runSize;
+                    else runSize = 0;
+                    yRuns[x, y] = runSize;
+                }
+            }
+            for (int y = 0; y < height; y++)
+            {
+                int runSize = 0;
+                for (int x = width - 1; x >= 0; x--)
+                {
+                    if (g.Filled(x, y)) ++runSize;
+                    else runSize = 0;
+                    xRuns[x, y] = runSize;
+                }
+            }
+            for (int x = 0; x < width; x++)
+                for (int y = 0; y < height; y++)
+                    claimed[x, y] = !g.Filled(x, y);
+        }
+
+        public bool NextRect(out Rect rect)
+        {
+            while (cY < height && claimed[cX, cY])
+            {
+                if (++cX == width)
+                {
+                    cX = 0;
+                    ++cY;
+                }
+            }
+
+            if (cY == height)
+            {
+                rect = default;
+                return false;
+            }
+
+            rect = BuildCurRect();
+            return true;
+        }
+
+        private Rect BuildCurRect()
+        {
+            // FIXME
+            return null;
+        }
+    }
+
+    public static class ColliderOptimizer
+    {
+        public static List<Rect> Covering(Grid grid)
+        {
+            var runs = new CoverageRuns(grid);
+            List<Rect> result = new List<Rect>();
+            while (runs.NextRect(out var rect)) result.Add(rect);
+
+            return result;
+        }
+    }
+}
---
+++ DarknestDungeonUnityScripts/Scripts/TilemapCompiler.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine.Tilemaps;
+using UnityEngine;
+
+namespace DarknestDungeon.Scripts
+{
+    [RequireComponent(typeof(Tilemap))]
+    public class TilemapCompiler : MonoBehaviour
+    {
+        [ContextMenu("Compile Tilemap")]
+        void CompileTilemap() {
+            var oldMap = gameObject.GetComponent<Tilemap>();
+            var width = oldMap.size.x;
+            var height = oldMap.size.y;
+
+            GameObject prevCompiled = gameObject.transform.Find("Compiled")?.gameObject;
+            if (prevCompiled != null) DestroyImmediate(prevCompiled, true);
+
+            GameObject compiled = new GameObject();
+            compiled.name = "Compiled";
+            compiled.transform.SetParent(gameObject.transform);
+        }
+    }
+}
