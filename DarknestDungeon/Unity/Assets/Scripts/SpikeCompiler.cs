19f52a18215716dfa41e1e937ff1f136a9f6efd4
dplochcoder <daniel.ploch.coder@gmail.com>
2022-12-17 23:08:12 -0500

    Start SpikeCompiler

---
+++ DarknestDungeon/Unity/Assets/Scripts/SpikeCompiler.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class SpikeCompiler : SceneDataOptimizer
+{
+    public override void OptimizeScene()
+    {
+#if UNITY_EDITOR
+        CompileSpikes();
+#endif
+    }
+
+#if UNITY_EDITOR
+    [ContextMenu("Compile Tilemap")]
+    void CompileSpikes()
+    {
+        // FIXME
+    }
+#endif
+}
